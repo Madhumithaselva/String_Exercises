6fca01d86a1a860284ecaf4f717dba8fe61c2545
Madhu <madhu8129@gmail.com>
2023-11-23 10:31:49 +0100

    Ex1 Updated:New string is added and the vaue at given index position is found

--- src/main/java/se/lexicon/StringLength.java
+++ src/main/java/se/lexicon/StringLength.java
@@ -5,5 +5,11 @@ public class StringLength {
         String str="Java";
         int strLength=str.length();
         System.out.println("Length of the given string(Java) is "+strLength);
+
+        //Code to find the index value from a string
+        str="Long example sentence";
+       // strLength=str.length();
+        System.out.println("New string is :"+str);
+        System.out.println("Character at index position 6 in new string is :"+str.charAt(6));
     }
 }
