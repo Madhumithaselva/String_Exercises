6cbc04cd04c1c58b8b7c5f1a27b66eae0d5bdf94
Madhu <madhu8129@gmail.com>
2023-11-23 11:16:59 +0100

    Ex3 created:Creating a substring from the given string

---
+++ src/main/java/se/lexicon/Ex3Substring.java
@@ -0,0 +1,13 @@
+package se.lexicon;
+
+public class Ex3Substring {
+    public static void main(String[] args){
+        //Creating a string with a value
+        String string="Ok this is not as long!";
+        //finding the position of Index
+        int position=string.indexOf('n');
+        //Getting the substring using index
+        String substring=string.substring(position,string.length()-1);
+        System.out.println(substring);
+    }
+}
