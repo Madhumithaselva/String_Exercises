9bffd32265d02f751de546c95b00218749e5d46a
Madhu <madhu8129@gmail.com>
2023-11-23 11:22:33 +0100

    Ex4 created:Lower case and Upper case

---
+++ src/main/java/se/lexicon/Ex4LowerCaseAndUpperCase.java
@@ -0,0 +1,11 @@
+package se.lexicon;
+
+public class Ex4LowerCaseAndUpperCase {
+    public static void main(String[] args){
+        String string="CAPS EQUALS SCREAMING";
+        string=string.toLowerCase();
+        System.out.println("To LowerCase:"+string);
+        string=string.toUpperCase();
+        System.out.println("To UpperCase= " + string);
+    }
+}
