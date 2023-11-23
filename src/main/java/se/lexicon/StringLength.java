174c44a782758e480c9db94081c5bf9ba719dcaf
Madhu <madhu8129@gmail.com>
2023-11-23 10:22:08 +0100

    Ex1: Program to find string length

---
+++ src/main/java/se/lexicon/StringLength.java
@@ -0,0 +1,9 @@
+package se.lexicon;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String str="Java";
+        int strLength=str.length();
+        System.out.println("Length of the given string(Java) is "+strLength);
+    }
+}
