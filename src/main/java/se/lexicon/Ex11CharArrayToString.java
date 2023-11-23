bdcd0ecc2e30d5f6a316e31ee16ed5e0fe381b48
Madhu <madhu8129@gmail.com>
2023-11-23 14:57:34 +0100

    Ex11 created: Char array to string conversion

---
+++ src/main/java/se/lexicon/Ex11CharArrayToString.java
@@ -0,0 +1,14 @@
+package se.lexicon;
+
+public class Ex11CharArrayToString {
+    public static void main(String[] args){
+        //Daclaring a ch array
+        char[] chArray={'J', 'a', 'v', 'a'};
+
+        //Assigning the char Array into a string
+        String string =new String(chArray);
+
+        //Display the string
+        System.out.println(string);
+    }
+}
