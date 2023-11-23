8330607a808e196277a2f351987dfea4c49e3608
Madhu <madhu8129@gmail.com>
2023-11-23 11:09:12 +0100

    Ex2 created:index position

---
+++ src/main/java/se/lexicon/Ex2IndexOfExample.java
@@ -0,0 +1,9 @@
+package se.lexicon;
+
+public class Ex2IndexOfExample {
+    public static void  main(String[] args){
+        String string="Even longer example sentence";
+        System.out.println("Index position of 'o' is: "+string.indexOf('o'));
+
+    }
+}
